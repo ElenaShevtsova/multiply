e220eab397658f47acc0f6dece2cc6b96eaec346
Elena <helena_shevts@mail.ru>
2019-10-08 13:51:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let multiplyOfNumbers = BigInt(first) * BigInt(second);
+  let result = String(multiplyOfNumbers);
+  return result;
 }
